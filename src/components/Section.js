9c8a2020cac9f7c90ee8996f677f188173c7df13
Mauro.V <remanomauro@gmail.com>
2022-10-18 15:47:26 +0200

    add button group

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,14 @@ function Section() {
             <h1>Model S</h1>
             <p>Order online for Tochless Delivery</p>
         </ItemText>
+        <ButtonGroup>
+          <LeftButton>
+          Custom Order
+          </LeftButton>
+          <RightButton>
+          Existing Inventory
+          </RightButton>
+        </ButtonGroup>
     </Wrap>
   )
 }
@@ -21,8 +29,34 @@ background-size: cover;
 background-position: center;
 background-repeat: no-repeat;
 background-image: url('/images/model-s.jpg');
+display: flex;
+flex-direction: column;
+justify-content: space-between; //vertical
+align-items: center; //horizontal
 `
 const ItemText = styled.div`
 padding-top: 15vh;
 text-align: center;
+`
+
+const ButtonGroup = styled.div`
+display:flex;
+margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+background-color: rgba(23, 26, 32, 0.8);
+height: 40px;
+width: 256px;
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 100px;
+opacity: 0.85;
+text-transform: uppercase;
+font-size: 12px;
+cursor: pointer;
+`
+const RightButton = styled(LeftButton)`
+
 `
